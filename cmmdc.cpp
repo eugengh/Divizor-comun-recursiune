f86b7c97ea9cc14a6a479c109804af6772ef545d
noname for the moment <70998165+eugengh@users.noreply.github.com>
2022-10-08 11:41:23 +0300

    Add files via upload

---
+++ cmmdc.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int cmmdc(int a, int b)
+
+{
+
+    if (a==b)
+
+        return a;
+
+    if (a>b)
+
+        return cmmdc(a-b, b);
+    else
+
+        return cmmdc(a, b-a);
+
+}
+
+int main()
+ {
+
+    int f;
+
+    f = cmmdc(18, 24);
+
+    cout<<f;
+
+    return 0;
+
+}
